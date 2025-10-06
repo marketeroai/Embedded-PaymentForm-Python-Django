0113cae74bc80d54aecbc161bc47e14f9fab35f6
creativomultimedia <steveagente@gmail.com>
2025-10-06 11:03:18 -0500

    Actualizados paquetes

---
+++ Keys/keys.py
@@ -0,0 +1,13 @@
+keys = {
+  # Identificador de su tienda
+  "USERNAME": "~ CHANGE_ME_USER_ID ~",
+
+  # Clave de Test o Producción
+  "PASSWORD": "~ CHANGE_ME_PASSWORD ~",
+
+  # Clave Pública de Test o Producción
+  "PUBLIC_KEY": "~ CHANGE_ME_PUBLIC_KEY ~",
+
+  # Clave HMAC-SHA-256 de Test o Producción
+  "HMACSHA256": "~ CHANGE_ME_HMAC_SHA_256 ~"
+}
